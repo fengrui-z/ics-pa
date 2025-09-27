3e83cb3c5bb2e2acf43d85f6291b10209032975d
tracer-ics2025 <tracer@njuics.org>
2025-09-27 08:28:14 +0000

    >  compile NEMU 602025330038 张峰瑞 Linux ip-172-31-42-75 6.14.0-1011-aws #11~24.04.1-Ubuntu SMP Fri Aug  1 02:07:25 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  08:28:14 up 15 days, 19:26,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/isa/x86/include/isa-def.h
+++ nemu/src/isa/x86/include/isa-def.h
@@ -27,18 +27,14 @@
  */
 
 typedef struct {
-  union{
-    union {
-      uint32_t _32;
-      uint16_t _16;
-      uint8_t _8[2];
-    } gpr[8];
-    struct {
-  /* Do NOT change the order of the GPRs' definitions. */
-      uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-    }
-  }
-  vaddr_t pc;
+  struct {
+    uint32_t _32;
+    uint16_t _16;
+    uint8_t _8[2];
+  } gpr[8];
+  /* Do NOT change the order of the GPRs' definitions. */
+  uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+  vaddr_t pc;
 } x86_CPU_state;
 
 // decode
